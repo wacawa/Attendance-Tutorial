dc63c15108645c4693f80603523d757a595cb94c
aki <mikkunmitsuaki@gmail.com>
2020-01-28 02:55:09 +0000

    ログインページ作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
+  
   def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.suthenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
   end
 end
