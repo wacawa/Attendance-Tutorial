da8b60d34ae9bfff162a362157a5acc840850557
aki <mikkunmitsuaki@gmail.com>
2020-02-01 05:07:40 +0000

    departmentカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200131121339) do
+ActiveRecord::Schema.define(version: 20200201044718) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200131121339) do
     t.string "password_digest"
     t.boolean "admin", default: false
     t.string "remember_digest"
+    t.string "department"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
