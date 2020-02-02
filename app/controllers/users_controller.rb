ba08848fe31e9f2c16d38d2899aa31e2ddafcf03
aki <mikkunmitsuaki@gmail.com>
2020-02-02 01:40:42 +0000

    出勤日数、在社時間の計算を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -10,4 +10,8 @@ module AttendancesHelper
     false
   end
   
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+  end
 end
