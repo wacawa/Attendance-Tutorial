c484573ead1b9d02b4a9670b1b5881d157d8f7a1
aki <mikkunmitsuaki@gmail.com>
2020-02-02 01:24:39 +0000

    出勤、退勤登録ボタン追加

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20200201095512) do
     t.boolean "admin", default: false
     t.string "remember_digest"
     t.string "department"
-    t.datetime "basic_time", default: "2020-01-31 23:00:00"
-    t.datetime "work_time", default: "2020-01-31 22:30:00"
+    t.datetime "basic_time", default: "2020-02-01 23:00:00"
+    t.datetime "work_time", default: "2020-02-01 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
